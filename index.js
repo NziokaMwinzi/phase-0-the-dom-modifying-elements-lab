2a4521bd4a4000b5b72b04dc17fb31afdc16c96e
daniel nzioka <dmwinzi50@gmail.com>
2024-07-26 15:36:12 +0300

    modifying

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Remove the existing main element
+const main = document.querySelector('main');
+if (main) {
+    main.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Dan is the champion';
+document.body.appendChild(newHeader);
